5cd2cdc94e3a57c7e7fcdd5392329be62b797161
LENOVO <LENOVO@128.4.200.212>
2014-10-06 19:42:02 -0700

    commit4

--- Poker_joker/src/Poker_joker/TestHands.java
+++ Poker_joker/src/Poker_joker/TestHands.java
@@ -1,13 +1,10 @@
 package Poker_joker;
 
+
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import poker.Card;
@@ -19,63 +16,143 @@ import poker.eSuit;
 
 public class TestHands {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
-	public final void TestRoyalFlush1() {
-
-		ArrayList<Card> rfHand = new ArrayList<Card>();
-		rfHand.add(new Card(eSuit.CLUBS,eRank.TEN));
-		rfHand.add(new Card(eSuit.CLUBS,eRank.JACK));
-		rfHand.add(new Card(eSuit.CLUBS,eRank.QUEEN));
-		rfHand.add(new Card(eSuit.CLUBS,eRank.KING));
-		rfHand.add(new Card(eSuit.CLUBS,eRank.ACE));
-		Hand h = Hand.EvalHand(rfHand);
-		
-		assertEquals("Should be rf:",eHandStrength.RoyalFlush.getHandStrength(),h.getHandStrength());
-		
-	}
-
-	@Test
-	public final void TestRoyalFlush2() {
+	public final void TestTwoJokers() {
 		
 		ArrayList<Card> rfHand = new ArrayList<Card>();
-		rfHand.add(new Card(eSuit.CLUBS,eRank.KING));
-		rfHand.add(new Card(eSuit.CLUBS,eRank.ACE));
-		rfHand.add(new Card(eSuit.CLUBS,eRank.TEN));
-		rfHand.add(new Card(eSuit.CLUBS,eRank.JACK));
-		rfHand.add(new Card(eSuit.CLUBS,eRank.QUEEN));
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
 		Hand h = Hand.EvalHand(rfHand);		
 		
-		assertEquals("Should be rf:",eHandStrength.RoyalFlush.getHandStrength(),h.getHandStrength());		
+		assertEquals("Should be 5K:",eHandStrength.FiveofAkind.getHandStrength(),h.getHandStrength());		
+	}
+
+	@Test
+	public final void FiveOfAKindWithTwoJokers() {
+		
+		ArrayList<Card> rfHand = new ArrayList<Card>();
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.EIGHT));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.FIVE));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.FOUR));
+
+		Hand h = Hand.EvalHand(rfHand);		
+		
+		assertEquals("Should be 5K:",eHandStrength.FiveofAkind.getHandStrength(),h.getHandStrength());		
+	}
+
+
+	
+	@Test
+	public final void TestTwoJokersFiveOfAKind() {
+		
+		ArrayList<Card> rfHand = new ArrayList<Card>();
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.FIVE));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.FIVE));
+		rfHand.add(new Card(eSuit.HEARTS,eRank.FIVE));
+		Hand h = Hand.EvalHand(rfHand);		
+		
+		assertEquals("Should be 5K:",eHandStrength.FiveofAkind.getHandStrength(),h.getHandStrength());		
 	}
 	
 	@Test
-	public final void TestStraightFlush1() {
+	public final void TestTwoJokersRoyalFlush() {
 		
 		ArrayList<Card> rfHand = new ArrayList<Card>();
-		rfHand.add(new Card(eSuit.CLUBS,eRank.KING));
-		rfHand.add(new Card(eSuit.CLUBS,eRank.TEN));
-		rfHand.add(new Card(eSuit.CLUBS,eRank.NINE));
-		rfHand.add(new Card(eSuit.CLUBS,eRank.JACK));
-		rfHand.add(new Card(eSuit.CLUBS,eRank.QUEEN));
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.ACE));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.KING));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.TEN));
 		Hand h = Hand.EvalHand(rfHand);		
 		
-		assertEquals("Should be Sf:",eHandStrength.StraightFlush.getHandStrength(),h.getHandStrength());		
+		assertEquals("Should be RF:",eHandStrength.RoyalFlush.getHandStrength(),h.getHandStrength());		
 	}
-
+	
+	@Test
+	public final void TestTwoJokersRoyalFlush1() {
+		
+		ArrayList<Card> rfHand = new ArrayList<Card>();
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.KING));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.JACK));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.TEN));
+		Hand h = Hand.EvalHand(rfHand);		
+		
+		assertEquals("Should be RF:",eHandStrength.RoyalFlush.getHandStrength(),h.getHandStrength());		
+	}
+	
+	@Test
+	public final void TestTwoJokersStraightFlush() {
+		
+		ArrayList<Card> rfHand = new ArrayList<Card>();
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.FOUR));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.THREE));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.TWO));
+		Hand h = Hand.EvalHand(rfHand);		
+		
+		assertEquals("Should be SF:",eHandStrength.StraightFlush.getHandStrength(),h.getHandStrength());		
+	}
+	
+	@Test
+	public final void FourOfAKindWithTwoJokers() {
+		
+		ArrayList<Card> rfHand = new ArrayList<Card>();
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.FOUR));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.EIGHT));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.EIGHT));
+		Hand h = Hand.EvalHand(rfHand);		
+		
+		assertEquals("Should be 4K:",eHandStrength.FourOfAKind.getHandStrength(),h.getHandStrength());		
+	}
+	
+	@Test
+	public final void FlushWithTwoJokers() {
+		
+		ArrayList<Card> rfHand = new ArrayList<Card>();
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.FOUR));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.EIGHT));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.THREE));
+		Hand h = Hand.EvalHand(rfHand);		
+		
+		assertEquals("Should be F:",eHandStrength.Flush.getHandStrength(),h.getHandStrength());		
+	}
+	
+	@Test
+	public final void StraightWithTwoJokers() {
+		
+		ArrayList<Card> rfHand = new ArrayList<Card>();
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.FIVE));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.FOUR));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.THREE));
+		Hand h = Hand.EvalHand(rfHand);		
+		
+		assertEquals("Should be S:",eHandStrength.Straight.getHandStrength(),h.getHandStrength());		
+	}
+	
+	@Test
+	public final void ThreeOfAKindWithTwoJokers() {
+		
+		ArrayList<Card> rfHand = new ArrayList<Card>();
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.Joker,eRank.JOKER));
+		rfHand.add(new Card(eSuit.HEARTS,eRank.FIVE));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.NINE));
+		rfHand.add(new Card(eSuit.DIAMONDS,eRank.THREE));
+		Hand h = Hand.EvalHand(rfHand);		
+		
+		assertEquals("Should be 3K:",eHandStrength.ThreeOfAKind.getHandStrength(),h.getHandStrength());		
+	}
 }
